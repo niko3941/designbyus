20d42fa858d4400b734374331f8992bacd1ed89c
Simone Hviid Poulsen <simone@Simones-MacBook-Pro.local>
2018-05-11 10:03:13 +0200

    json ændringer

--- produkterJS.js
+++ produkterJS.js
@@ -2,7 +2,7 @@
         async function hentMenu() {
             let topMenu = await fetch("navigation.html");
             let menu = await topMenu.text();
-            document.querySelector("[data-navigation]").innerHTML = menu;
+            //document.querySelector("[data-navigation]").innerHTML = menu;
         }
 
         let produkter;
@@ -27,36 +27,23 @@
                 klon.querySelector("[data-produktpic]").src = produkt.acf.main_photo.url;
                 klon.querySelector("[data-produktnavn]").textContent = produkt.acf.product_name;
                 klon.querySelector("[data-produktbeskrivelse]").innerHTML = produkt.acf.product_description;
-                klon.querySelector("[data-produktknap1]").href = "produktmodtager.html?id=" + produkt.id;
-                klon.querySelector("[data-produktknap2]").href = "produktmodtager.html?id=" + produkt.id;
-                klon.querySelector("[data-produktknap3]").href = "produktmodtager.html?id=" + produkt.id;
+                klon.querySelector("[data-produktknap1]").href = "produktmodtag.html?id=" + produkt.id;
+                klon.querySelector("[data-produktknap2]").href = "produktmodtag.html?id=" + produkt.id;
+                klon.querySelector("[data-produktknap3]").href = "produktmodtag.html?id=" + produkt.id;
+                klon.querySelector("[data-produktpris]").innerHTML = produkt.acf.price + " DKK";
                 //klon.querySelector("[data-produktknap4]").href = "produktmodtager.html?id=" + produkt.id;
                 templateModtager.appendChild(klon);
             })
         }
 
-        //HENT ENKELT PRODUKT PÅ SINGLEPAGE SIDE
 
-        async function hentJson() {
-            let url = new URL(window.location.href);
-            let parameter = new URLSearchParams(url.search);
-            let id = parameter.get("id");
-            let jsonData = await fetch("http://simonepoulsen.dk/kea/2semester/wordpress/wp-json/wp/v2/products/" + id);
-            let single = await jsonData.json();
-            document.querySelector("[data-produktpic]").src = single.acf.main_photo.url;
-            document.querySelector("[data-produktnavn]").textContent = single.acf.productname;
-            document.querySelector("[data-produktbeskrivelse]").innerHTML = single.acf.beskrivelse;
-            document.querySelector("[data-produktspecifikation]").innerHTML = produkt.acf.specifications;
-            document.querySelector("[data-produktpris]").innerHTML = produkt.acf.price;
-            document.querySelector("[data-produktfarve]").innerHTML = produkt.acf.color;
-        }
 
         //Footer fra footer.html hentes
         async function hentFooter() {
             let footer = await fetch("footer.html");
             let footerIndhold = await footer.text();
 
-            document.querySelector("[data-footer]").innerHTML = footerIndhold;
+            //document.querySelector("[data-footer]").innerHTML = footerIndhold;
         }
 
         //hent alt indhold når siden er loaded
---
+++ produktmodtagJS.js
@@ -0,0 +1,17 @@
+        //HENT ENKELT PRODUKT PÅ SINGLEPAGE SIDE
+        async function hentJson() {
+            let url = new URL(window.location.href);
+            let parameter = new URLSearchParams(url.search);
+            let id = parameter.get("id");
+            let jsonData = await fetch("http://simonepoulsen.dk/kea/2semester/wordpress/wp-json/wp/v2/products/" + id);
+            let single = await jsonData.json();
+            document.querySelector("[data-produktpic]").src = single.acf.main_photo.url;
+            document.querySelector("[data-produktnavn]").textContent = single.acf.product_name;
+            document.querySelector("[data-produktbeskrivelse]").innerHTML = single.acf.product_description;
+            document.querySelector("[data-produktspecifikation]").innerHTML = single.acf.specifications;
+            document.querySelector("[data-produktpris]").innerHTML = single.acf.price + " DKK";
+            document.querySelector("[data-produktfarve]").innerHTML = single.acf.color;
+        }
+
+        //hent alt indhold når siden er loaded
+        document.addEventListener("DOMContentLoaded", hentJson);
