a1d34816d470f210bffe36ac30ae180f7f92343b
Simone Hviid Poulsen <simone@Simones-MacBook-Pro.local>
2018-05-13 15:05:47 +0200

    En fejl i json linket er rettet, så produkterne på singlesiden igen bliver vist

--- produktmodtagJS.js
+++ produktmodtagJS.js
@@ -3,7 +3,7 @@
             let url = new URL(window.location.href);
             let parameter = new URLSearchParams(url.search);
             let id = parameter.get("id");
-            let jsonData = await fetch("http://simonepoulsen.dk/kea/2semester/wordpress/wp-json/wp/v2/products?per_page=100/" + id);
+            let jsonData = await fetch("http://simonepoulsen.dk/kea/2semester/wordpress/wp-json/wp/v2/products/" + id);
             let single = await jsonData.json();
             document.querySelector("[data-produktpic]").src = single.acf.main_photo.url;
             document.querySelector("[data-produktnavn]").textContent = single.acf.product_name;
